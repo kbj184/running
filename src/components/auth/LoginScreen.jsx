76e021e6c79e13edd46bb637f25904f93eebd148
kbj184 <gsskbjss@gmail.com>
2025-12-26 20:04:28 +0900

    Refactor auth styles: move layout to LoginScreen.css, adjust import order

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './auth.css';
+import './LoginScreen.css';
 import { api } from '../../utils/api';
 
 const LoginScreen = ({ onLogin }) => {
