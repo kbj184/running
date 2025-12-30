8f9bd1f5145d819db47133144d98689273a21aae
kbj184 <gsskbjss@gmail.com>
2025-12-30 23:04:00 +0900

    Refactor: '내 기록' 탭 제목 제거 (사용자 요청)

--- src/components/profile/MyRecordsTab.jsx
+++ src/components/profile/MyRecordsTab.jsx
@@ -4,7 +4,6 @@ import RecentRecords from '../common/RecentRecords';
 function MyRecordsTab({ refreshRecords, onRecordClick }) {
     return (
         <div>
-            <h2 style={{ marginBottom: '20px', fontSize: '20px', fontWeight: '700' }}>내 기록</h2>
             <RecentRecords
                 onRefresh={refreshRecords}
                 onRecordClick={onRecordClick}
